f5dfa3466818a98de639ef51f7fbdf78a86618b0
dddmadison <119731457+dddmadison@users.noreply.github.com>
2025-05-14 00:05:02 +0900

    app.py

--- app.py
+++ app.py
@@ -51,5 +51,7 @@ def index():
                                    dogcat_class=f"{pred_label} ({confidence:.1f}%)")
     return render_template("index.html")
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render가 자동 지정한 포트를 읽음
+    app.run(host="0.0.0.0", port=port)
